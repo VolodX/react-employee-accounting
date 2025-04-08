5ceebf7f619b15c02ed41bcb50b2fbefebc6d5c8
VolodX <xpain1996@gmail.com>
2025-04-08 12:57:33 +0300

    feat: add app-info component

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h2>Accounting of employees in the EMP company</h2>
+      <h2>Total number of employees: </h2>
+      <h2>Prizes will be awarded to: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
