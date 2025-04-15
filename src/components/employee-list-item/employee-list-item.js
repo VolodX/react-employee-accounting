467dbae720192ee57e38e8cb55b8d9c70e44ee12
VolodX <xpain1996@gmail.com>
2025-04-15 18:18:49 +0300

    feat: add star toggle to mark employee for promotion

--- src/components/employee-list-item/employee-list-item.js
+++ src/components/employee-list-item/employee-list-item.js
@@ -1,31 +1,61 @@
+import {Component} from 'react';
 import './employee-list-item.css';
 import cn from 'classnames';
 
-const EmployeeListItem = ({name, salary, increase}) => {
-  const classes = cn('list-group-item', 'd-flex', 'justify-content-between', {
-    increase
-  });
+class EmployeeListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+			rise: false
+    };
+  }
 
-  return (
-    <li className={classes}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + '$'}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+  onIncrease = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }));
+  };
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+	onRise = () => {
+		this.setState(({rise}) => ({
+			rise: !rise
+		}))
+	}
+
+  render() {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+    const classes = cn('list-group-item', 'd-flex', 'justify-content-between', {
+      increase,
+			rise
+    });
+
+    return (
+      <li className={classes}>
+        <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + '$'}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.onIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeeListItem;
