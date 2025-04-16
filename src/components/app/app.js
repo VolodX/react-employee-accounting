f6ebc40479a37055153db72cdb21a1fce7bbb604
VolodX <xpain1996@gmail.com>
2025-04-16 16:47:09 +0300

    refactor(App): convert component from functional to class-based

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import {Component} from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,43 @@ import EmployeeAddForm from '../employee-add-form/employee-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 1400, increase: false, id: 1},
+        {name: 'Alex M.', salary: 4600, increase: true, id: 2},
+        {name: 'Carl W.', salary: 9500, increase: false, id: 3},
+        {name: 'Mark.F', salary: 9900, increase: false, id: 4}
+      ]
+    };
+  }
 
-	const data = [
-		{name: 'John C.', salary: 1400, increase: false, id: 1},
-		{name: 'Alex M.', salary: 4600, increase: true, id: 2},
-		{name: 'Carl W.', salary: 9500, increase: false, id: 3},
-		{name: 'Mark.F', salary: 9900, increase: false, id: 4},
-	]
+	deleteItem = (id) => {
+		this.setState(({data}) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
 
-  return (
-    <div className="app">
-      <AppInfo />
+		})
+	}
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeeList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeeAddForm />
       </div>
-
-      <EmployeeList data={data}/>
-			<EmployeeAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
