08cc7b9d04ad319a7436e88efc8a57c116997d04
VolodX <xpain1996@gmail.com>
2025-05-06 20:56:27 +0300

    feat(employee-add-form): improve employee form with optimizations and validation

--- src/components/employee-add-form/employee-add-form.js
+++ src/components/employee-add-form/employee-add-form.js
@@ -2,27 +2,41 @@ import {Component} from 'react';
 import './employee-add-form.css';
 
 class EmployeeAddForm extends Component {
-	state = {
-		name: '',
-		salary: ''
-	};
-
-  onValueChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  state = {
+    name: '',
+    salary: ''
   };
 
-  onSubmit = e => {
+  // handleValueChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // };
+
+	handleValueChange = e => {
+    const {name, value} = e.target;
+    
+    if (name === 'salary') {
+      if (/^\d*$/.test(value)) {
+        this.setState({[name]: value});
+      }
+    } else {
+      // For all other fields, work as before
+      this.setState({[name]: value});
+    }
+  };
+	
+  handleSubmit = e => {
     e.preventDefault();
-		const salaryValue = parseInt(this.state.salary);
-		const nameValue = this.state.name;
+    const nameValue = this.state.name.trim();
+    const salaryValue = parseInt(this.state.salary, 10);
 
     if (nameValue.length >= 3 && !isNaN(salaryValue) && salaryValue >= 1) {
-			this.props.onAdd(nameValue, salaryValue);
-		} else {
-			return;
-		}
+      this.props.onAdd(nameValue, salaryValue);
+    } else {
+      return;
+    }
+
     this.setState({
       name: '',
       salary: ''
@@ -35,23 +49,26 @@ class EmployeeAddForm extends Component {
     return (
       <div className="app-add-form">
         <h3>Add a new employee</h3>
-        <form className="add-form d-flex" onSubmit={this.onSubmit}>
+        <form className="add-form d-flex" onSubmit={this.handleSubmit}>
           <input
             type="text"
             className="form-control new-post-label"
             placeholder="What's his name?"
             name="name"
             value={name}
-            onChange={this.onValueChange}
+            onChange={this.handleValueChange}
           />
           <input
-            type="number"
-            className="form-control new-post-label"
-            placeholder="Salary in $?"
-            name="salary"
-            value={salary}
-            onChange={this.onValueChange}
-          />
+						type="text"
+						className="form-control new-post-label"
+						placeholder="Salary in $?"
+						name="salary"
+						value={salary}
+						onChange={this.handleValueChange}
+						maxLength={12}
+						pattern="[0-9]*"
+						inputMode="numeric"
+					/>
 
           <button type="submit" className="btn btn-outline-light">
             Add
